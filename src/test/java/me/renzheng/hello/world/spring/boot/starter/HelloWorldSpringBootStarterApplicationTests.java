132d6fa3ec44e6cd041bdf78672e0a60f96c80e3
Renzheng Zhang <renzheng@renzheng.me>
2023-03-20 23:03:17 +0800

    init repo.

---
+++ src/test/java/me/renzheng/hello/world/spring/boot/starter/HelloWorldSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.renzheng.hello.world.spring.boot.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
